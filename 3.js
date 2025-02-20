bb723adfdab2106b70af52b525f2cf2c05911012
doramaidink <vqthanh1999@gmail.com>
2025-02-20 22:09:17 +0700

    add

--- 3.js
+++ 3.js
@@ -1,15 +1,5 @@
 //js dành cho các nút dowm
 
-// const listtheloai = document.querySelector('.heading-1');
-// const down = document.querySelector('.down img');
-// function show(){
-//     listtheloai.classList.add('open');
-// }
-// function closeshow(){
-//    listtheloai.classList.remove('open');
-// }
-//  down.addEventListener('click', () => show());
-// //  down.addEventListener('click', () => closeshow());
 const listtheloai = document.querySelector('.heading-1');
 const down = document.querySelector('.down img');
 down.addEventListener('click', () => {
