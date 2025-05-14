ca7d08ecdbdd238a353695a7b8f13b1f2f1f7350
doramaidink <vqthanh1999@gmail.com>
2025-05-14 09:47:17 +0700

    add

--- kiemduyet.js
+++ kiemduyet.js
@@ -1,7 +1,7 @@
 
 function setActive(clickedButton) {
     // Xóa class 'active' khỏi tất cả nút
-    document.querySelectorAll('.btn-list').forEach(button => button.classList.remove('active'));
+    document.querySelectorAll('.btn-list').forEach(button => button.classList.remove('.active'));
     // Thêm class 'active' cho nút được click
-    clickedButton.classList.add('active');
+    clickedButton.classList.add('.active');
 }
